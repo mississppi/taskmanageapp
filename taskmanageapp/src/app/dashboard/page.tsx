1f90069d450b693835d0684f025e4dab2c67c2be
mississppi <msspstudio@gmail.com>
2024-10-14 11:11:02 +0900

    ログイン状態の判定のリダイレクト追加

--- taskmanageapp/src/app/dashboard/page.tsx
+++ taskmanageapp/src/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 import "../globals.css";
 import MainPanel from '@/components/MainPanel';
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    // サーバーサイドでセッション情報を取得
+    const session = await getServerSession(authOptions);
+    // セッションが存在しない場合は /login へリダイレクト
+    if (!session) {
+        redirect('/login');
+    }
+
     return (
         <MainPanel  />
     );
--- taskmanageapp/src/components/Header.tsx
+++ taskmanageapp/src/components/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { signOut } from 'next-auth/react';
 
 const Header = () => {
     return (
-        <header className="py-4 px-6 bg-gray-800 text-white">
-            <h1 className="text-xl font-bold">tasks</h1>
+        <header className="py-4 px-6 bg-gray-800 text-white flex justify-between items-center">
+            <h1 className="text-xl font-bold">Tasks</h1>
+            <button 
+                className="bg-red-500 text-white py-2 px-4 rounded"
+                onClick={() => signOut()}
+            >
+                ログアウト
+            </button>
         </header>
     );
 };
