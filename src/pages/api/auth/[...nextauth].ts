0dd042930cdb289d42cfa0046dcc7f5706bc63fd
mississppi <msspstudio@gmail.com>
2024-10-16 23:27:38 +0900

    authのエラー解消

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth";
+import { Session, User} from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -10,11 +12,11 @@ export const authOptions = {
     ],
     secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
-        async session({ session, token, user }) {
-        // セッションにカスタムデータを追加したい場合はここで処理
-        return session;
+        async session({ session, token, user }: { session: Session; token: JWT; user: User }) {
+            // セッションにカスタムデータを追加したい場合はここで処理
+            return session;
         },
-        async redirect({url, baseUrl}) {
+        async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
             return `${baseUrl}/dashboard`;
         }
     },
